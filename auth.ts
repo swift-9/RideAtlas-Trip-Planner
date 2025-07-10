3538bfb30769520277443be64bccd5e99c3cd969
Swastik Poojary <swastikjayaraj500@gmail.com>
2025-07-10 23:19:32 +0530

    Fix auth config and environment setup for production

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 
-export const { auth, handlers, signIn , signOut } = NextAuth({
-    providers: [Github],
-    adapter: PrismaAdapter(prisma),
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [Github],
+  adapter: PrismaAdapter(prisma),
 });
