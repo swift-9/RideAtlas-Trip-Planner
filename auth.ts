8eb02635c6e93507a95861e7ade9f14c5be335f6
Swastik Poojary <swastikjayaraj500@gmail.com>
2025-07-10 23:31:40 +0530

    ✅ Finalize GitHub OAuth and Vercel deployment configs with Prisma adapter and UploadThing support

--- auth.ts
+++ auth.ts
@@ -1,9 +1,21 @@
 import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [Github],
   adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 });
